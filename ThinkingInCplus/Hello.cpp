80d9b9a2ba552ea9902c2f00a63210e144ca2dcf
tuan dang anh <degea9@gmail.com>
2014-11-29 21:11:41 +0700

    add stream2

--- ThinkingInCplus/Hello.cpp
+++ ThinkingInCplus/Hello.cpp
@@ -8,10 +8,24 @@
 // Saying Hello with C++
 #include <iostream> // Stream declarations
 using namespace std;
+int stream2();
 
 int main() {
 cout << "Hello, World! I am "
 << 8 << " Today!" << endl;
+stream2();
 }
 
+int stream2(){
+	cout << "a number in decimal: "
+	<< dec << 15 << endl;
+	cout << "in octal: " << oct << 15 << endl;
+	cout << "in hex: " << hex << 15 << endl;
+	cout << "a floating-point number: "
+			<< 3.14159 << endl;
+			cout << "non-printing char (escape): "
+			<< char(27) << endl;
+
+	return 0;
+}
 
