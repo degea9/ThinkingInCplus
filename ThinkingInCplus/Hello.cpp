f1979e94c65c5166cca9e9be556b679539cfada2
tuan dang anh <degea9@gmail.com>
2014-11-29 20:49:49 +0700

    complete hello.cpp

--- ThinkingInCplus/Hello.cpp
+++ ThinkingInCplus/Hello.cpp
@@ -4,7 +4,14 @@
  *  Created on: Nov 29, 2014
  *      Author: tuandang
  */
+//: C02:Hello.cpp
+// Saying Hello with C++
+#include <iostream> // Stream declarations
+using namespace std;
 
-
+int main() {
+cout << "Hello, World! I am "
+<< 8 << " Today!" << endl;
+}
 
 
