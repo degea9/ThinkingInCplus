84d16fc8c17de1eb4f6e954324ecd5bf0e43184f
tuan dang anh <degea9@gmail.com>
2014-11-29 21:14:11 +0700

    add concat

--- ThinkingInCplus/Hello.cpp
+++ ThinkingInCplus/Hello.cpp
@@ -9,23 +9,26 @@
 #include <iostream> // Stream declarations
 using namespace std;
 int stream2();
+void concat();
 
 int main() {
-cout << "Hello, World! I am "
-<< 8 << " Today!" << endl;
-stream2();
+	cout << "Hello, World! I am " << 8 << " Today!" << endl;
+	stream2();
 }
 
-int stream2(){
-	cout << "a number in decimal: "
-	<< dec << 15 << endl;
+int stream2() {
+	cout << "a number in decimal: " << dec << 15 << endl;
 	cout << "in octal: " << oct << 15 << endl;
 	cout << "in hex: " << hex << 15 << endl;
-	cout << "a floating-point number: "
-			<< 3.14159 << endl;
-			cout << "non-printing char (escape): "
-			<< char(27) << endl;
-
+	cout << "a floating-point number: " << 3.14159 << endl;
+	cout << "non-printing char (escape): " << char(27) << endl;
 	return 0;
 }
 
+void concat() {
+	cout << "This is far too long to put on a "
+			"single line but it can be broken up with "
+			"no ill effects\nas long as there is no "
+			"punctuation separating adjacent character "
+			"arrays.\n";
+}
